fa3f9b68fc920833f4640810528996441b8c8e76
snehchauhan <snehpc0@gmail.com>
2026-09-03 17:23:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
       page.classList.add("is-open");
 
-    }, 2000);
+    }, 450);
 
 
     /* ================= REMOVE DOORS ================= */
